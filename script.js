cf92525d204b5242522e8a65ec150f94042da95d
hebert28 <HebertMejia28@gmail.com>
2021-04-26 22:21:32 -0500

    funcionalidad del menu responsive

--- script.js
+++ script.js
@@ -1,33 +1,63 @@
 
-
-document.querySelectorAll(".menu-icon")[0].addEventListener("click", function(){
-    console.log('funciona');
-
-    document.querySelectorAll(".menu-items")[0].style.display='block'
-    document.querySelectorAll(".menu-icon")[0].style.display='none'
-
-  
-})
+function myFunction(x) {
+    if (x.matches) { // If media query matches
 
 
-document.querySelectorAll(".exit-menu-icon")[0].addEventListener("click", function(){
-    console.log('funciona');
+        document.querySelectorAll(".menu-items")[0].style.display='block'
+        document.querySelectorAll(".menu-icon")[0].style.display='none'
 
-    document.querySelectorAll(".menu-items")[0].style.display='none'
-    document.querySelectorAll(".menu-icon")[0].style.display='block'
+        const item = document.querySelectorAll(".item-link");
+        
+        for (let i = 0; i < item.length; i++) {
+            item[i].addEventListener("click", function() {
+               document.querySelectorAll(".menu-items")[0].style.display='block'
+               document.querySelectorAll(".menu-icon")[0].style.display='none'
+            });
+        } 
 
-  
-})
-
-
-const item = document.querySelectorAll(".item-link");
-
- for (let i = 0; i < item.length; i++) {
-     item[i].addEventListener("click", function() {
+    
+         
+    } else {
         document.querySelectorAll(".menu-items")[0].style.display='none'
-        document.querySelectorAll(".menu-icon")[0].style.display='block'
-     });
- }
+        document.querySelectorAll(".menu-icon")[0].style.display='block'        
+
+        document.querySelectorAll(".menu-icon")[0].addEventListener("click", function(){
+            console.log('funciona');
+        
+            document.querySelectorAll(".menu-items")[0].style.display='block'
+            document.querySelectorAll(".menu-icon")[0].style.display='none'
+        
+          
+        })
+        
+        
+        document.querySelectorAll(".exit-menu-icon")[0].addEventListener("click", function(){
+            console.log('funciona');
+        
+            document.querySelectorAll(".menu-items")[0].style.display='none'
+            document.querySelectorAll(".menu-icon")[0].style.display='block'
+        
+          
+        })
+        
+        
+        const item = document.querySelectorAll(".item-link");
+        
+         for (let i = 0; i < item.length; i++) {
+             item[i].addEventListener("click", function() {
+                document.querySelectorAll(".menu-items")[0].style.display='none'
+                document.querySelectorAll(".menu-icon")[0].style.display='block'
+             });
+         }  
+  
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 1024px)")
+  myFunction(x) // Call listener function at run time
+  x.addEventListener("change", () => {
+   myFunction(x);
+});
 
 
 
